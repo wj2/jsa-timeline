5a5e33e3bbe08f6a8ae499c7447b3fcfb047a10c
wjj <williamjjohnston@gmail.com>
2015-11-19 21:34:53 -0600

    added simple events

--- make_site.py
+++ make_site.py
@@ -1,13 +1,12 @@
 
 import timeline_objects as to
 
-root_folder = 'tl_site'
-timeline_name = 'root.html'
+root_folder = 'events'
 
-def make_site(root, outname):
+def make_site(root, outname='index.html'):
      root_t = to.Timeline(root)
      root_t.save_html(outname)
 
 if __name__ == '__main__':
-    make_site(root_folder, timeline_name)
+    make_site(root_folder)
     
