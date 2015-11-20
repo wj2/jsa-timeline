d3ba345afc36240f51a959113e549a42b395644b
wjj <williamjjohnston@gmail.com>
2015-11-20 13:25:46 -0600

    support for multiple events and subevents per file using event and subevent and subsubevent tags

--- make_site.py
+++ make_site.py
@@ -4,7 +4,7 @@ import timeline_objects as to
 root_event = 'index.txt'
 
 def make_site(root, outname='index.html'):
-     root_t = to.Event(root)
+     root_t = to.Event(path=root)
      root_t.save_html(outname)
 
 if __name__ == '__main__':
--- timeline_objects.py
+++ timeline_objects.py
@@ -47,30 +47,100 @@ def make_event_date(date):
         pass
     return '{} {}, {}'.format(month, date, year)
 
+def read_multiple_events(path=None, linelist=None, headersymbol='%%', 
+                         expectlines=6, evbegin='event', prevpath=None):
+    if path is not None:
+        f = open(path, 'rb')
+        linelist = [x for x in f.readlines()]
+    else:
+        path = prevpath
+    events = []
+    prev_lines = []
+    lhs = len(headersymbol)
+    for i, line in enumerate(linelist):
+        head = line[:lhs] == headersymbol 
+        event_beg = line[lhs:].strip(' ').lower()[:-1] == evbegin
+        if head and event_beg:
+            if len(prev_lines) >= expectlines:
+                e = Event(linelist=prev_lines, prevpath=path, ind=len(events),
+                          level=evbegin)
+                events.append(e)
+                prev_lines = []
+        prev_lines.append(line)
+    e = Event(linelist=prev_lines, prevpath=path, ind=len(events), 
+              level=evbegin)
+    events.append(e)
+    return events
+            
 class Event(object):
 
-    def __init__(self, path, headersymbol='%%'):
-        f = open(path, 'rb')
+    def __init__(self, linelist=None, path=None, headersymbol='%%',
+                 prevpath=None, ind=None, level='event'):
+        if path is not None:
+            f = open(path, 'rb')
+            linelist = [x for x in f.readlines()]
+            loc = os.path.splitext(path)[0]
+            self.html_path = loc + '.html'
+        else: 
+            prevp = os.path.splitext(prevpath)[0]
+            h_path = '{}_{}.html'.format(prevp, ind)
+            self.html_path = h_path
         descript = ''
-        for i, line in enumerate(f.readlines()):
-            if line[:len(headersymbol)] == headersymbol:
-                param, val = split_and_strip(line[len(headersymbol):], ':')
-                setattr(self, param.lower().strip('\n'), 
-                        val.lower().strip('\n'))
+        sublevel = 'sub'+level
+        for i, line in enumerate(linelist):
+            lhs = len(headersymbol)
+            if line[:lhs] == headersymbol:
+                # param, val = split_and_strip(line[lhs:], ':')
+                out = split_and_strip(line[lhs:], ':')
+                if len(out) == 2:
+                    param, val = out
+                    setattr(self, param.lower().strip('\n'), 
+                            val.lower().strip('\n'))
+                elif out[0][:-1].lower() == sublevel:
+                    break
             else:
                 descript = descript + line
         self.description = descript
-        self.mkd_path = path
-        loc = os.path.splitext(path)[0]
-        self.html_path = loc + '.html'
+        if i + 1 < len(linelist):
+            es = read_multiple_events(linelist=linelist[i:], headersymbol=headersymbol,
+                                      evbegin=sublevel, prevpath=self.html_path)
+            self._add_events_to_timeline(es)
+            
+    def _add_events_to_timeline(self, events):
+        t = self.get_timeline()
+        if t is None:
+            t = Timeline(event_list=events, html_name=self._html_name)
+        else:
+            t.add_events(events)
+        self.timeline = t
+
+    @property
+    def _html_name(self):
+        hname = os.path.splitext(self.html_path)[0]
+        return hname
+
+    def get_timeline(self):
+        try:
+            t = self.timeline
+        except AttributeError:
+            t = None
+        else:
+            try: 
+                t.upper()
+            except AttributeError:
+                pass
+            else:
+                self.timeline = Timeline(source_folder=t, 
+                                         html_name=self._html_name)
+                t = self.timeline
+        return t
+        
 
     def get_html(self):
-        try: 
-            t_fold = self.timeline
-        except AttributeError:
+        t = self.get_timeline()
+        if t is None:
             pretitle = ''
         else:
-            t = Timeline(t_fold)
             pretitle = t.get_html()
         desc = mkd.markdown(self.description)
         e_start = make_event_date(self.start)
@@ -111,24 +181,41 @@ class Event(object):
         
 class Timeline(object):
 
-    def __init__(self, source_folder, eventpatt='.*\.txt$', html_name='index'):
+    def __init__(self, source_folder=None, event_list=None, 
+                 eventpatt='.*\.txt$', html_name='index'):
+        self.html_path = os.path.splitext(html_name)[0] + '.html'
+        if source_folder is not None:
+            events = self._make_timeline_folder(source_folder, 
+                                                eventpatt=eventpatt)
+        elif event_list is not None:
+            events = event_list
+        self.events = events
+
+    def _make_timeline_folder(self, source_folder, eventpatt='.*\.txt$'):
         event_files = os.listdir(source_folder)
         event_files = filter(lambda x: re.match(eventpatt, x) is not None, 
                              event_files)
         event_files = [os.path.join(source_folder, x) for x in event_files]
-        self.html_path = os.path.splitext(html_name)[0] + '.html'
-        self.events = []
+        events = []
         for f in event_files:
-            ev = Event(f)
-            self.events.append(ev)
+            # ev = Event(path=f)
+            # events.append(ev)
+            evs = read_multiple_events(path=f)
+            events = events + evs
+        return events
+
+    def add_events(self, events):
+        self.events = self.events + events
 
     def get_html(self):
         entries = []
         row_links = []
         for e in self.events:
             link = e.save_html()
+            rellink = os.path.relpath(link, 
+                                      os.path.split(self.html_path)[0])
             entries = entries + e.get_table_entries()
-            row_links = row_links + [link]*len(entries)
+            row_links = row_links + [rellink]*len(entries)
         e_arr = '[' + ','.join(entries) + ']'
         thtml = timeline_template.substitute(row_array=e_arr, 
                                              event_links=row_links)
